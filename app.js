0d4729b517f79e7f31bd8becb5c075416e988458
Denys Kaspryshen <rootofthemail@gmail.com>
2020-11-04 21:31:11 +0200

    final

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/posts', require('./routes/posts.routes'))
 app.use('/api/user', require('./routes/user.routes'))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = config.get('port') || 5000
 
 async function start() {
